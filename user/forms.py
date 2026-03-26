51a7c7246c0d40c5cd2d51b6293b1eefdebb5677
young-faber <yng.fbr@gmail.com>
2026-03-26 19:08:12 +0300

    изменения в фронтенд

--- user/forms.py
+++ user/forms.py
@@ -11,3 +11,14 @@ class RegistrForm(UserCreationForm):
     class Meta: 
         model = MyUser
         fields = ['username', 'email', 'password1', 'password2']
+        labels = {
+            'username':'имя пользователя',
+            'email':'email',
+            'password':'введите пароль',
+            'password2':'потвердите пароль'
+        }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['password1'].label = 'введите пароль'
+        self.fields['password2'].label = 'потвердите пароль'
